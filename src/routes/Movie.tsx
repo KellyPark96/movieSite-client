fec0c58686ebbdb47c27ef552d763552d2e7cc54
Kyeongeun Park <kellypark2032@gmail.com>
2024-02-27 18:25:31 +0900

    style: movies, movie.tsx style 적용

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { useParams } from "react-router-dom";
 
 const GET_MOVIE = gql`
@@ -6,6 +8,8 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       id
       title
+      medium_cover_image
+      rating
     }
   }
 `;
@@ -17,8 +21,53 @@ export default function Movie() {
       movieId: id,
     },
   });
-  if (loading) {
-    return <h1>Fetching movie...</h1>;
-  }
-  return <div>{data.movie.title}</div>;
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+      </Column>
+      <Image backgroundImage={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+export const backgroundImage = (props: { backgroundImage: string }) =>
+  css`
+    background-image: url(${props.backgroundImage});
+  `;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+  ${backgroundImage}
+`;
