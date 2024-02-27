937972820fc50ed1a1880ea4ff47ad2b908508c8
Kyeongeun Park <kellypark2032@gmail.com>
2024-02-27 23:32:21 +0900

    feat: add routes named login profile createAccount

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,64 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
 import Movies from "./routes/Movies";
 import Movie from "./routes/Movie";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/CreateAccount";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+  },
+  {
+    path: "/movies/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Movies />} />
-        <Route path="/movies/:id" element={<Movie />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
   );
 }
