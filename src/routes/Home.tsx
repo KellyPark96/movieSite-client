7822628cd3ac6d9327431b70dcdfa1910d5cf88e
Kyeongeun Park <kellypark2032@gmail.com>
2024-03-01 18:35:49 +0900

    style: :lipstick: edit layout.tsx style

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,3 @@
-import { auth } from "../firebase";
-
 export default function Home() {
-  const onClickLogout = () => {
-    auth.signOut();
-  };
-
-  return (
-    <h1>
-      <button onClick={onClickLogout}>LogOut</button>
-    </h1>
-  );
+  return <h1></h1>;
 }
