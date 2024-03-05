e24b4aa6672c362f5730955141cf5d716d67e08e
Kyeongeun Park <kellypark2032@gmail.com>
2024-03-05 15:51:54 +0900

    feat: delete tweet

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,5 @@
 import {
   collection,
-  getDocs,
   limit,
   onSnapshot,
   orderBy,
@@ -23,76 +22,38 @@ export interface ITweet {
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    const spanshot = await getDocs(tweetsQuery);
-    const tweets = spanshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
   useEffect(() => {
-    // let unsubscribe: Unsubscribe | null = null;
-    // const fetchTweets = async () => {
-    //   const tweetsQuery = query(
-    //     collection(db, "tweets"),
-    //     orderBy("createdAt", "desc"),
-    //     limit(25)
-    //   );
-    //   unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
-    //     const tweets = snapshot.docs.map((doc) => {
-    //       const { tweet, createdAt, userId, username, photo } = doc.data();
-    //       return {
-    //         tweet,
-    //         createdAt,
-    //         userId,
-    //         username,
-    //         photo,
-    //         id: doc.id,
-    //       };
-    //     });
-    //     setTweet(tweets);
-    //   });
-    // };
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
     fetchTweets();
-    // return () => {
-    //   unsubscribe && unsubscribe();
-    // };
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
-
-  const testTweets = [
-    {
-      tweet: "1",
-      createdAt: "2015-02-15",
-      userId: "1",
-      username: "kelly",
-      photo:
-        "https://cdnetphoto.appphotocard.com/boards/172/2153004/6747735/c8e530d1917580d4e5ba8767f1b83dcb.jpg",
-      id: "1",
-    },
-    {
-      tweet: "1",
-      createdAt: "2015-02-15",
-      userId: "1",
-      username: "kelly",
-      id: "2",
-    },
-  ];
 
   return (
     <Wrapper>
-      {testTweets.map((tweet) => (
+      {tweets.map((tweet) => (
         <Tweet key={tweet.id} {...tweet} />
       ))}
     </Wrapper>
@@ -103,4 +64,5 @@ const Wrapper = styled.div`
   display: flex;
   gap: 10px;
   flex-direction: column;
+  overflow-y: scroll;
 `;
