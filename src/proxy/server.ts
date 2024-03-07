4321bebbbbeddd6fe90703ed8db984193ec78427
Kyeongeun Park <kellypark2032@gmail.com>
2024-03-07 18:56:04 +0900

    Revert "feat: server.ts middleware test"
    This reverts commit 126d189fc555287bc952a0b094583008c7c34586.

--- src/proxy/server.ts
+++ src/proxy/server.ts
@@ -1,18 +1,20 @@
-import express, { Express } from "express";
-import morgan from "morgan";
-import globalRouter from "../routes/globalRouter.ts";
-import videoRouter from "../routes/videoRouter.ts";
-import userRouter from "../routes/userRouter.ts";
+import express, { Express, Request, Response } from "express";
+import { NextFunction } from "express-serve-static-core";
 
 const PORT: number = 4000;
 
 const app: Express = express();
-const logger = morgan("dev");
-app.use(logger);
 
-app.use("/", globalRouter);
-app.use("/videos", videoRouter);
-app.use("/users", userRouter);
+const gossipMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
+const handleHome = (_req: Request, res: Response) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () => {
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
