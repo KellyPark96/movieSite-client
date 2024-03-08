7dc73f50cb283a7c84491e565f1c6a28cd140902
Kyeongeun Park <kellypark2032@gmail.com>
2024-03-08 17:06:05 +0900

    feat: connect vieo model with postUpload

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
-import Video from "../models/Video.ts";
+import Video, { videoSchemaTypeProps } from "../models/Video.ts";
 
 export const home = async (_req: Request, res: Response) => {
   const videos = await Video.find({});
   console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (_req: Request, res: Response) => {
-  return res.render("watch", { pageTitle: "Watching" });
+export const watch = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const video: videoSchemaTypeProps | null = await Video.findById(id);
+  return res.render("watch", { pageTitle: video?.title, video });
 };
 export const getEdit = (_req: Request, res: Response) => {
   return res.render("edit", { pageTitle: "Editing" });
@@ -19,6 +21,24 @@ export const postEdit = (req: Request, res: Response) => {
 export const getUpload = (_req: Request, res: Response) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (_req: Request, res: Response) => {
-  return res.redirect("/");
+export const postUpload = async (req: Request, res: Response) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title: title,
+      description: description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(",").map((word: string) => `#${word}`),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error: any) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error.message,
+    });
+  }
 };
